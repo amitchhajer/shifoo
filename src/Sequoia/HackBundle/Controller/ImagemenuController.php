1234020e6409da7b1c23eaca94e35c9d5da903e0
J Kishore kumar <justjkk@gmail.com>
2014-09-20 16:57:02 +0530

    first commit

---
+++ src/Sequoia/HackBundle/Controller/ImagemenuController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sequoia\HackBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\View\View;
+use FOS\Rest\Util\Codes;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class ImagemenuController extends Controller
+{
+	public function getImagemenuAction()
+	{
+		var_dump('test');die;
+	}
+}
