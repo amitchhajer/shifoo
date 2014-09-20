dedc92ea86aff712377d7762e21c9c57c52cbbc6
Amit Chhajer <amitchhajer@gmail.com>
2014-09-21 05:08:00 +0530

    800 percent optimized code

--- src/Sequoia/HackBundle/Controller/ImagemenuController.php
+++ src/Sequoia/HackBundle/Controller/ImagemenuController.php
@@ -53,54 +53,22 @@ class ImagemenuController extends Controller
 
             $fields = 'nf_ingredient_statement,nf_calories,nf_total_fat,nf_serving_weight_grams';
 
+            $id = 0;
             foreach ($words as $word) {
                 if (!in_array($word, $blackListedKeys)) {
                     if (intval($word)) {
                         $combinedWord = trim($combinedWord);
-                        $id = time();
                         $items[$id]['id'] = $id;
                         $items[$id]['name'] = $combinedWord;
 
                         $combinedWord = urlencode($combinedWord);
-                        $ch = curl_init();
-                        $url = "https://api.nutritionix.com/v1_1/search/$combinedWord?results=0:1&fields=item_name,nf_ingredient_statement,nf_calories,nf_total_fat,nf_serving_weight_grams&appId=$nAppId&appKey=$nAppKey";
 
-                        curl_setopt($ch,CURLOPT_URL, $url);
-                        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+                        $nutritionUrls[] = "https://api.nutritionix.com/v1_1/search/$combinedWord?results=0:1&fields=item_name,nf_ingredient_statement,nf_calories,nf_total_fat,nf_serving_weight_grams&appId=$nAppId&appKey=$nAppKey";
 
-                        $output = curl_exec($ch);
-                        $output = null;
-
-                        if ($output) {
-                            $result = json_decode($output,true);
-                            $fields = $result['hits'][0]['fields'];
-
-                            $items[$id]['item_name'] = $fields['item_name'];
-                            $items[$id]['ingredients'] = $fields['nf_ingredient_statement'];
-                            $items[$id]['calories'] = $fields['nf_calories'];
-                            $items[$id]['fat'] = $fields['nf_total_fat'];
-                            $items[$id]['serving_weight_grams'] = $fields['nf_serving_weight_grams'];
-                        }
-
-                        $ch = curl_init();
-                        curl_setopt($ch,CURLOPT_URL,"https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=$combinedWord&imgsz=large");
-                        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-
-                        $output = curl_exec($ch);
-                        if ($output) {
-                            $result = json_decode($output, true);
-                            $results = $result['responseData']['results'];
-                            $i = 0;
-                            foreach ($results as $result) {
-                                $items[$id]['image_urls'][] = $result['url'];
-                                $i = $i + 1;
-                                if ($i >= 4) {
-                                    break;
-                                }
-                            }
-                        }
+                        $googleUrls[] = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=$combinedWord&imgsz=large";
 
                         $combinedWord = null;
+                        $id = $id + 1;
                         continue;
                     } else {
                         $combinedWord .= ' '.$word;
@@ -108,6 +76,73 @@ class ImagemenuController extends Controller
                 }
             }
 
+            $googleData = $this->multiCurl($googleUrls);
+            $nutritionData = $this->multiCurl($nutritionUrls);
+
+            $id = 0;
+            foreach ($googleData as $googleDatum) {
+                if ($googleDatum) {
+                    $result = json_decode($googleDatum[0], true);
+                    $results = $result['responseData']['results'];
+                    $i = 0;
+                    foreach ($results as $result) {
+                        $items[$id]['image_urls'][] = $result['url'];
+                        $i = $i + 1;
+                        if ($i >= 4) {
+                            break;
+                        }
+                    }
+                }
+                $id = $id + 1;
+            }
+
+            $id = 0;
+            foreach ($nutritionData as $nutritionDatum) {
+                if ($nutritionDatum) {
+                    $result = json_decode($nutritionDatum[0],true);
+                    $fields = $result['hits'][0]['fields'];
+
+                    $items[$id]['item_name'] = $fields['item_name'];
+                    $items[$id]['ingredients'] = $fields['nf_ingredient_statement'];
+                    $items[$id]['calories'] = $fields['nf_calories'];
+                    $items[$id]['fat'] = $fields['nf_total_fat'];
+                    $items[$id]['serving_weight_grams'] = $fields['nf_serving_weight_grams'];
+                }
+                $id = $id + 1;
+            }
+
             return View::create(array_values($items));
+    }
+
+    /**
+    * Takes array of URL's
+    */
+    protected function multiCurl($urls)
+    {
+        $nodes = $urls;
+        $nodeCount = count($nodes);
+
+        $curlArr = array();
+        $master = curl_multi_init();
+
+        for($i = 0; $i < $nodeCount; $i++)
+        {
+            $url = $nodes[$i];
+            $curlArr[$i] = curl_init($url);
+            curl_setopt($curlArr[$i], CURLOPT_RETURNTRANSFER, true);
+            curl_multi_add_handle($master, $curlArr[$i]);
+        }
+
+        do {
+            curl_multi_exec($master,$running);
+        } while($running > 0);
+
+
+        for($i = 0; $i < $nodeCount; $i++)
+        {
+            $results[$i][] = curl_multi_getcontent($curlArr[$i]);
+        }
+
+        return $results;
     }
 }
